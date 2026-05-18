1adc2a2c82f9487aa6208e2b7b37a26aa4309a8a
degger8 <degger8@tsn.at>
2026-05-18 14:03:25 +0200

    Basics CSS text + background

--- php/index.php
+++ php/index.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test </title>
-    <link rel="stylesheet" href="./CSS/stylesheet.css">
+    <link rel="stylesheet" href="../CSS/stylesheet.css">
   </head>
   <body>
     <main>
-        <h1>Test</h1>  
+        <h1 class = 'text'>Test</h1>  
+        <div class = 'leftBack location'> 
+          <button class = 'buttonStyle'> Hello! </button>
+        </div>
+        <h1 class = 'leftBack location'> Test Links </h1>
+        <h1 class = 'rightBack location'> Test Rechts </h1>
     </main>
   </body>
 </html>
