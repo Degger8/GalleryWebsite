589e2001eba1c577a4e9c50277913757dd50ffcf
degger8 <degger8@tsn.at>
2026-06-15 14:19:45 +0200

    Fertiges Stylesheet/CSS-Layout.

--- php/index.php
+++ php/index.php
@@ -10,13 +10,35 @@
     <main>
       <div class = 'layout'>
         <div class = 'leftBack location'> 
-          <div class = 'buttonPlacement'>
-            <button class = 'buttonStyle'> Hello! </button>
+          <h1 class = 'text'> Datenbank </h1> 
+          <button class = 'buttonStyle'> DBManager </button>          
+          
+          <h1 class = 'text'> Gallerien</h1>
+          <div class = 'scrollableElement text'>
+            <h1> Gallerie1 </h1>
+            <button class = 'buttonStyle'> öffnen </button>
+            <h1> Gallerie2 </h1>
+            <button class = 'buttonStyle'> öffnen </button>
+            <h1> Gallerie3 </h1>
+            <button class = 'buttonStyle'> öffnen </button>
+            <h1> Gallerie4 </h1>
+            <button class = 'buttonStyle'> öffnen </button>
           </div>
-          <h1 class = 'text titlePlacement'> Test Links </h1>
+
         </div>
         <div class = 'rightBack location'>
-          <h1 class = 'text titlePlacement'> Test Rechts </h1>
+          <h1 class = 'text'> Bilder </h1>
+          <div class = 'imageScroll'>
+            <img class = 'imageSize' src = 'https://www.justspices.de/media/opti_image/webp/recipe/resized/510x510/recipe/Fleisch_und_Braten_Gewuerz_Klassisches_Wiener_Schnitzel-9.webp'>
+            <img class = 'imageSize' src = 'https://thomassixt.de/wp-content/uploads/2020/12/wiener_schnitzel_glaenzende_panade.jpg.webp' width = 400px height = 500px>
+            <img class = 'imageSize' src = 'https://thomassixt.de/wp-content/uploads/2020/12/wiener_schnitzel_glaenzende_panade.jpg.webp' width = 200px height = 100px>
+            <img class = 'imageSize' src = 'https://thomassixt.de/wp-content/uploads/2020/12/wiener_schnitzel_glaenzende_panade.jpg.webp' width = 200px height = 100px>
+            <img class = 'imageSize' src = 'https://thomassixt.de/wp-content/uploads/2020/12/wiener_schnitzel_glaenzende_panade.jpg.webp' width = 200px height = 100px>
+            <img class = 'imageSize' src = 'https://thomassixt.de/wp-content/uploads/2020/12/wiener_schnitzel_glaenzende_panade.jpg.webp' width = 200px height = 100px>
+            <img class = 'imageSize' src = 'https://thomassixt.de/wp-content/uploads/2020/12/wiener_schnitzel_glaenzende_panade.jpg.webp' width = 200px height = 100px>
+            <img class = 'imageSize' src = 'https://thomassixt.de/wp-content/uploads/2020/12/wiener_schnitzel_glaenzende_panade.jpg.webp' width = 200px height = 100px>
+            <img class = 'imageSize' src = 'https://die-frau-am-grill.de/wp-content/uploads/wiener-schnitzel-rezept.jpg.webp'>
+          </div>
         </div>
       </div>    
     </main>
